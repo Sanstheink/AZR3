8d131a653ac9df7eccc34178854848e73c3807cf
Pemmy1729 <hmae5558@gmail.com>
2026-01-30 23:51:26 +0700

    Create data.js

---
+++ assets/js/data.js
@@ -0,0 +1,26 @@
+const AUTH={
+ "AZURA-10092":{
+  name:"Eren Maveria",
+  id:"10092",
+  rank:"Crewman",
+  position:"Navigation",
+  ship:"USS AZURA NCC-1656-G",
+  agency:"Oceana Operation Central"
+ },
+ "AZURA-CAPT-01":{
+  name:"Catherine Mabel",
+  id:"0001",
+  rank:"Captain",
+  position:"Commanding Officer",
+  ship:"USS AZURA NCC-1656-G",
+  agency:"United Federation of Planet Ambassador"
+ },
+  "ADMR-UFP-01":{
+  name:"Origa manabo",
+  id:"01902",
+  rank:"Admiral",
+  position:"Operation HQ",
+  ship:"HQ Spacedock",
+  agency:"Oceana Academy"
+ },
+};
